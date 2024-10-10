2006757b7cd3915f5f5e282004c8b9e80830d640
singhjaspreet97 <jassi80singh80@gmail.com>
2024-10-10 12:45:40 -0230

    Sidebar code added

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -17,16 +17,16 @@ const Topbar = () => {
 
     return (<Box display="flex" justifyContent="space-between" p={2}>
         { /* SEARCH BAR */}
-        <box 
-            display="flex" 
-            backgroundColor={colors.primary[400]} 
+        <Box
+            display="flex"
+            backgroundColor={colors.primary[400]}
             borderRadius="3px"
         >
-            <InputBase sx={{ ml: 2, flex: 1}} placeholder="Search" />
+            <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
             <IconButton type="button" sx={{ p: 1 }}>
-                <SearchIcon />
+            <SearchIcon />
             </IconButton>
-        </box>
+        </Box>
 
 
         { /* ICONS */}
